5d945fa898cac9527ca56c84f9e3d767d501c9f3
GBsouza9 <giovanna.bsouza09@gmail.com>
2025-10-30 10:09:37 -0300

    feat(header): criar componente de cabeçalho responsivo com menu, logo e ícones

--- front-end/src/app/header/header.component.ts
+++ front-end/src/app/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
